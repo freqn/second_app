89d7f486309f8c922f41b2d685c5210bca2dc1ac
Chip Eyler <ceyler@gmail.com>
2014-01-13 20:20:52 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HartlApp::Application.config.secret_key_base = 'bea6c6ec5c13a9e4909da6a140701a379616cca7f425d6fe38805c74a304baf41b0c98e82304bebf00ac1387ddc3451d277213727102a52551f7df876880b9b4'
